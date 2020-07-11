6fde4d900d1c4cb975a5eac10cf92e872ae37ee3
Mrunmayee Talwalkar <mrunmayee.talwalkar@ad.infosys.com>
2020-07-11 17:34:16 +0530

    Updates

---
+++ precache-manifest.103dba96c994e43c758cd8639b0c0fa1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543109e14057f6ac480b433da363c37d",
+    "url": "/Shuttrbug/index.html"
+  },
+  {
+    "revision": "a885489cfc0893c70876",
+    "url": "/Shuttrbug/static/css/main.a6c0650d.chunk.css"
+  },
+  {
+    "revision": "d87d37a4873f95998885",
+    "url": "/Shuttrbug/static/js/2.9bd8462e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Shuttrbug/static/js/2.9bd8462e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a885489cfc0893c70876",
+    "url": "/Shuttrbug/static/js/main.e01f3383.chunk.js"
+  },
+  {
+    "revision": "78bd26cb3efc8718fc9a",
+    "url": "/Shuttrbug/static/js/runtime-main.e3822ad6.js"
+  },
+  {
+    "revision": "61cc8a99941923a0b6a746b19e2bc753",
+    "url": "/Shuttrbug/static/media/banner_1.61cc8a99.jpg"
+  },
+  {
+    "revision": "4ab9785d4627d663cfbf2b553eb38ea1",
+    "url": "/Shuttrbug/static/media/banner_2.4ab9785d.jpg"
+  },
+  {
+    "revision": "769ae3339cb0a1b4ca069e1241e15fc8",
+    "url": "/Shuttrbug/static/media/logo_transparent.769ae333.png"
+  },
+  {
+    "revision": "2f64ca46759e0c4a7debaecfe2c21583",
+    "url": "/Shuttrbug/static/media/mplus-1p-medium.2f64ca46.ttf"
+  },
+  {
+    "revision": "16159c5f8d8bed43944cc7c4111e9bf8",
+    "url": "/Shuttrbug/static/media/mplus-1p-regular.16159c5f.ttf"
+  }
+]);
